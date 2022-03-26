e71fdac75be00b9b0e3603231a108c94081a3a86
miro-puetz <miropuetz@gmail.com>
2022-03-26 16:20:05 +0100

    added logging and CLI support

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	server "github.com/miro-puetz/http-server/src"
@@ -12,6 +14,18 @@ var addr string = ":8080"
 
 func main() {
 
+	logFilename := flag.String("logFilename", "", "log output to given file name or leave empty for standard output")
+
+	flag.Parse()
+
+	if *logFilename != "" {
+		file, err := os.OpenFile(*logFilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.SetOutput(file)
+	}
+
 	srv := &http.Server{
 		Addr:              addr,
 		Handler:           http.HandlerFunc(server.DefaultHandler),
@@ -19,5 +33,6 @@ func main() {
 		ReadHeaderTimeout: time.Minute,
 	}
 
+	log.Println("Server started")
 	log.Fatal(srv.ListenAndServe())
 }
